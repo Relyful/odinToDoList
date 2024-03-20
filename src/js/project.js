69db0e92d5f98d657295d23b9dcb0e0792c113e8
Relyful <13381270+Relyful@users.noreply.github.com>
2024-03-20 21:37:26 +0100

    Create basic grid layout, add allProjects class that keeps track of all projects.

--- src/js/project.js
+++ src/js/project.js
@@ -1,9 +1,11 @@
 import toDo from './toDo.js';
 
 export default class project {
+    static projectCounter = 0;
     toDoList = [];
     constructor(name) {
         this.name = name;
+        this.index = project.projectCounter++;
     }
 
     addTodo(title, description, dueDate, priority) {
